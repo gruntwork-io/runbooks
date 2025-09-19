663b6406571536e2a5b883b80c62ff2fd58abf77
Josh Padnick <josh@gruntwork.io>
2025-09-19 15:15:33 -0700

    Add watch subcommand. Hide help and completion subcommands.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # runbooks
+
+...
+
+## Hidden subcommands
+
+### completion
+Set up shell auto-completion for bash, zsh, fish, and PowerShell. Use `runbooks completion --help` for setup instructions.
+
--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -9,8 +8,6 @@ import (
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -46,6 +43,10 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// Hide the completion command from help
+	rootCmd.CompletionOptions.HiddenDefaultCmd = true
+
+	// Hide the help subcommand from help
+	rootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
 }
-
-
