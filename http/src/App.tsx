ebf41804745e54ccf46ce78f9c23720820007136
Josh Padnick <josh@gruntwork.io>
2025-09-19 17:58:37 -0700

    Clean up base CSS

--- http/src/App.tsx
+++ http/src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>Gruntwork Runbooks</h1>
-      <p>This is a placeholder for the runbook consumer.</p>
+      <div className="flex flex-col items-center justify-center pt-8">
+        <h1>Gruntwork Runbooks</h1>
+        <p>This is a placeholder for the runbook consumer.</p>
+      </div>
     </>
   )
 }
