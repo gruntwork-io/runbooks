730a4c5e59359341ff9365eb0d2176296bca9888
Josh Padnick <josh@gruntwork.io>
2025-10-09 16:24:18 -0700

    Add About dialog

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,3 +1,13 @@
+import { useState } from 'react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from './ui/alert-dialog';
+
 interface HeaderProps {
   pathName: string;
 }
@@ -12,19 +22,52 @@ interface HeaderProps {
  * @param props.pathName - The file path string to display in the center of the header
  */
 export function Header({ pathName }: HeaderProps) {
+  const [isAboutDialogOpen, setIsAboutDialogOpen] = useState(false);
+
   return (
-    <header className="w-full border-b border-gray-300 p-4 text-gray-500 font-semibold flex fixed top-0 left-0 right-0 z-10 bg-bg-default">
-      <div className="hidden md:block md:absolute md:left-5 md:top-1/2 md:transform md:-translate-y-1/2">
-        Gruntwork Runbooks
-      </div>
-      <div className="flex-1 flex items-center gap-2 justify-center">
-        <div className="text-xs md:text-sm text-gray-500 font-mono font-normal">
-          {pathName}
+    <>
+      <header className="w-full border-b border-gray-300 p-4 text-gray-500 font-semibold flex fixed top-0 left-0 right-0 z-10 bg-bg-default">
+        <div className="hidden md:block md:absolute md:left-5 md:top-1/2 md:transform md:-translate-y-1/2">
+          Gruntwork Runbooks
         </div>
-      </div>
-      <div className="hidden md:block md:absolute md:right-5 md:top-1/2 md:transform md:-translate-y-1/2 font-normal text-md hover:underline decoration-current">
-        <a href="https://gruntwork.io" target="_blank">About</a>
-      </div>
-    </header>
+        <div className="flex-1 flex items-center gap-2 justify-center">
+          <div className="text-xs md:text-sm text-gray-500 font-mono font-normal">
+            {pathName}
+          </div>
+        </div>
+        <div className="hidden md:block md:absolute md:right-5 md:top-1/2 md:transform md:-translate-y-1/2 font-normal text-md hover:underline decoration-current">
+          <button 
+            onClick={() => setIsAboutDialogOpen(true)}
+            className="cursor-pointer"
+          >
+            About
+          </button>
+        </div>
+      </header>
+
+      <AlertDialog open={isAboutDialogOpen} onOpenChange={setIsAboutDialogOpen}>
+        <AlertDialogContent>
+          <div className="relative">
+            <AlertDialogHeader>
+              <AlertDialogTitle>Gruntwork Runbooks</AlertDialogTitle>
+              <AlertDialogDescription className="text-left">
+                Runbooks enable DevOps subject matter experts to capture and share their expertise in a way that is easy to understand and use.
+                <br /><br />
+                Runbooks is published by <a target="_blank" href="https://gruntwork.io">Gruntwork</a> and is <a target="_blank" href="https://github.com/gruntwork-io/runbooks">open source</a>!
+                <AlertDialogAction className="block mt-4" onClick={() => setIsAboutDialogOpen(false)}>
+                Close
+                </AlertDialogAction>
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <img 
+              src="/grunty_with_runbooks.png" 
+              alt="Grunty with runbooks" 
+              className="absolute -bottom-15 -right-12 w-25 h-25 md:-bottom-10 md:-right-15 md:w-30 md:h-30 object-contain drop-shadow-lg"
+            />
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 }
+
