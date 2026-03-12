4910f81d3a1d0d72fa0a9c2951ad07ca172cb57b
Josh Padnick <josh@gruntwork.io>
2026-03-12 13:03:56 -0700

    Code cleanup

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://runbooks.gruntwork.io',
-integrations: [
+	integrations: [
 		starlight({
 			title: 'Gruntwork Runbooks',
 			logo: {
