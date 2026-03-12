555260e3018b85e8344c75838fb0f990ffe8a9ed
Josh Padnick <josh@gruntwork.io>
2026-03-12 11:17:45 -0700

    Continuing to iterate on homepage

---
+++ testdata/sample-runbooks/homepage-demo/checks/node-version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Check that Node.js is installed and meets the minimum version
+node_version=$(node --version 2>/dev/null)
+if [ $? -ne 0 ]; then
+  echo "Node.js is not installed"
+  exit 1
+fi
+echo "Node.js $node_version is installed"
